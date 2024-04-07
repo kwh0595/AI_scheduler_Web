81a77d057394fd81b0e355661aaf820c4b3914e1
kim woo hee <rladngml2002@naver.com>
2024-04-07 18:33:06 +0900

    Initial commit

---
+++ src/main/java/com/capstone/timeflow/TimeflowApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.timeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/timeflow/TimeflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.timeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
