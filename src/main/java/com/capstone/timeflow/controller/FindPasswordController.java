0ef2f0079ea64029b523b55bb1d2dd9ceedeba52
cydungo <133199865+cydungo@users.noreply.github.com>
2024-04-14 23:23:44 +0900

    Add files via upload

---
+++ src/main/java/com/capstone/timeflow/controller/FindPasswordController.java
@@ -0,0 +1,23 @@
+package com.capstone.timeflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FindPasswordController {
+    // 비밀번호 찾기 페이지 요청
+    @GetMapping("/find-password")
+    public String findPasswordForm() {
+        return "findPassword";
+    }
+    @PostMapping("/find-password")
+    public String findPassword(@RequestParam("Id") String userId,
+                               @RequestParam("phone") String phone
+    ) {
+        System.out.println("MemberController.signup");
+        System.out.println("userId = " + userId + ", phone = " + phone);
+        return "login";
+    }
+}
