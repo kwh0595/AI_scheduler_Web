28f1419727fd048aa10e97f1e0d77f0bc21ac506
kim woo hee <rladngml2002@naver.com>
2024-05-04 02:13:58 +0900

    Add passwordEncode

--- src/main/java/com/capstone/timeflow/repository/UserRepository.java
+++ src/main/java/com/capstone/timeflow/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.capstone.timeflow.repository;
 
 import com.capstone.timeflow.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 }
