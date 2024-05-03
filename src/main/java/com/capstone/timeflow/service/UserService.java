14c8eb78e96e9d2dfc66a221746d08d9a159d387
kim woo hee <rladngml2002@naver.com>
2024-05-04 01:31:32 +0900

    Add passwordEncode

--- src/main/java/com/capstone/timeflow/service/UserService.java
+++ src/main/java/com/capstone/timeflow/service/UserService.java
@@ -5,14 +5,24 @@ import com.capstone.timeflow.entity.UserEntity;
 import com.capstone.timeflow.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-   private final UserRepository userRepository;
+    private  final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 유저 저장 메서드
     public void save(UserDTO userDTO) {
         UserEntity userEntity = UserEntity.toUserEntity(userDTO);
-        userRepository.save(userEntity);
+
+        //비밀번호 암호화
+        String encodedPassword = bCryptPasswordEncoder.encode(userDTO.getUserPassword());
+        userEntity.setUserPassword(encodedPassword); //암호화된 비밀번호를 엔티티에 저장
+
+        userRepository.save(userEntity); //사용자 레포지토리에 암호화된 비밀번호 저장
     }
 }
